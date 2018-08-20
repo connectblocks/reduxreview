89ccbc5f17cbe82a7ff0f9ce4f72daa03ad4f92b
michael <michael.shin.hrr@gmail.com>
2018-08-20 01:42:14 -0700

    set up the router

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Single extends React.Component {
+    render(){
+        return (
+            <div className="photo-grid">
+        I am the pg
+            </div>
+        )
+
+    }
+}
+
+export default Single;
---
+++ client/components/Single.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Single extends React.Component {
+    render(){
+        return (
+            <div className="single-photo">
+       i am single
+            </div>
+        )
+
+    }
+}
+
+export default Single;
