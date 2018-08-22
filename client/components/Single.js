786ef7e57534d1f4a906f1fdab87bc1e3cb0b9c1
michael <michael.shin.hrr@gmail.com>
2018-08-22 01:03:49 -0700

    implemented the add and remove comment add

--- client/components/Single.js
+++ client/components/Single.js
@@ -4,13 +4,15 @@ import Comments from './Comments';
 
 class Single extends React.Component {
     render(){
-        const i = this.props.posts.findIndex(post => post.code === this.props.params.postId);
-        const post = this.props.posts[i];
+        const i = this.props.posts.findIndex((post) => {
+            return post.code === this.props.params.postId;
+        });
+        const postComments = this.props.comments[this.props.params.postId] || []
 
         return (
             <div className="single-photo">
-                <Photo i={i} post={post} {...this.props} />
-                <Comments />
+                <Photo i={i} post={this.props.posts[i]} {...this.props} />
+                <Comments postComments={postComments} {...this.props}/>
             </div>
         )
     }
--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,28 @@
+function postComments( state = [], action) {
+    if(action.type === 'ADD_COMMENT') {
+        return [
+            ...state,{
+                user: action.author,
+                text: action.comments
+            }
+        ]
+    } else if (action.type === 'REMOVE_COMMENT') {
+        return [
+            ...state.slice(0, action.i),
+            ...state.slice(action.i + 1)
+        ]
+    } else {
+        return state;
+    }
+}
+
 function comments( state = [], action) {
-    console.log('comment', state, action);
+    if(typeof action.postId !== 'undefined') {
+        return {
+            ...state,
+            [action.postId]: postComments(state[action.postId], action)
+        }
+    }
     return state;
 }
 
